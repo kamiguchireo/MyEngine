a8d6abe64076d17376788ce2f1222c2adab20814
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2020-11-11 10:08:43 +0900

    GameTimeクラスの中身を実装

--- MyGame/MyGame/SourceFile/Timer/GameTime.h
+++ MyGame/MyGame/SourceFile/Timer/GameTime.h
@@ -1,5 +1,38 @@
 #pragma once
+#include "StopWatch.h"
+#include <list>
+
 class GameTime
 {
+public:
+	GameTime(){}
+	~GameTime(){}
+
+	//1フレームの経過時間を取得
+	const float GetFrameDeltaTime()const
+	{
+		return m_frameDeltaTime;
+	}
+
+	void PushFrameDeltaTime(float deltaTime)
+	{
+		m_frameDeltaTimeQue.push_back(deltaTime);
+		if (m_frameDeltaTimeQue.size() > 5)
+		{
+			float totalTime = 0.0f;
+			for (auto time : m_frameDeltaTimeQue)
+			{
+				totalTime += time;
+			}
+			//平均値をとる
+			m_frameDeltaTime = min(1.0f / 30.0f, totalTime / m_frameDeltaTimeQue.size());
+			//先頭要素を削除
+			m_frameDeltaTimeQue.pop_front();
+		}
+	}
+private:
+	std::list<float> m_frameDeltaTimeQue;
+	float m_frameDeltaTime = 1.0f / 60.0f;		//1フレームの経過時間
 };
 
+extern GameTime g_gameTime;
