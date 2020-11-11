3cdf43d408e20c9b624e5d9af4c04ebfb20fce0d
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2020-11-11 10:01:42 +0900

    StopWatchクラスの中身を実装

--- MyGame/MyGame/SourceFile/Timer/StopWatch.cpp
+++ MyGame/MyGame/SourceFile/Timer/StopWatch.cpp
@@ -1,2 +1,12 @@
 #include "stdafx.h"
 #include "StopWatch.h"
+
+//åvë™èIóπ
+void StopWatch::Stop()
+{
+	::QueryPerformanceFrequency(reinterpret_cast<LARGE_INTEGER*>(&freq));
+	::QueryPerformanceCounter(reinterpret_cast<LARGE_INTEGER*>(&end));
+	elapsed = double(end - begin) / freq;
+	elapsedMill = elapsed * 1000.0f;
+	elapsedMicro = elapsedMill * 1000.0f;
+}
--- MyGame/MyGame/SourceFile/Timer/StopWatch.h
+++ MyGame/MyGame/SourceFile/Timer/StopWatch.h
@@ -1,5 +1,54 @@
 #pragma once
 class StopWatch
 {
+public:
+	StopWatch()
+	{
+		freq = 0;
+		end = 0;
+		begin = 0;
+		elapsed = 0.0;
+		elapsedMill = 0.0;
+		elapsedMicro = 0.0;
+	}
+	~StopWatch()
+	{
+
+	}
+
+	//計測開始
+	void Start()
+	{
+		::QueryPerformanceCounter(reinterpret_cast <LARGE_INTEGER*>(&begin));
+	}
+
+	//計測終了
+	void Stop();
+
+	//経過時間を取得(単位:秒)
+	double GetElapsed() const
+	{
+		return elapsed;
+	}
+
+	//経過時間を取得(単位:ミリ秒)
+	double GetElapsedMillisecond() const
+	{
+		return elapsedMill;
+	}
+
+	//経過時間を取得(単位:マイクロ秒)
+	double GetElapsedMicrosecond() const
+	{
+		return elapsedMicro;
+	}
+
+private:
+	LONGLONG freq;			//周波数
+	LONGLONG end;			//終了
+	LONGLONG begin;			//開始
+	double elapsed;			//経過時間(単位 : 秒)
+	double elapsedMill;		//経過時間(単位 : ミリ秒)
+	double elapsedMicro;	//経過時間(単位 : マイクロ秒)
 };
 
