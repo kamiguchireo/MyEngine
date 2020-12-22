a61ced33ec86f82e40b5d0fc354d5f2c82eda919
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2020-12-22 14:13:20 +0900

    カメラの調整

--- MyGame/MyGame/Game/Camera/CameraStateTPS.h
+++ MyGame/MyGame/Game/Camera/CameraStateTPS.h
@@ -10,8 +10,8 @@ public:
 	void Update(Vector3& pos, Vector3& target);
 
 private:
-	float CameraDist = 300.0f;			//カメラからターゲットまでの距離
+	float CameraDist = 200.0f;			//カメラからターゲットまでの距離
 	float rot = 0.0f;			//回転量
-	float AddTargetPosY = 0.0f;		//ターゲットに加算する上方向の値
+	float AddTargetPosY = 35.0f;		//ターゲットに加算する上方向の値
 };
 
